7eb9eea34effe29fba08a75d041c7d9475c411ad
Vladimir Smelov <vladimirfol@gmail.com>
2023-02-08 13:25:12 +0700

    audit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
-const accounts = require('../accounts');
+// const accounts = require('../accounts');
 
 const networks = {
     localhost: {
@@ -10,31 +10,31 @@ const networks = {
         url: "https://rpc.ankr.com/eth/6c2f34a42715fa4c50762b0069a7a658618c752709b7db32f7bfe442741117eb",
         chainId: 1,
         gasPrice: 25000000000,
-        accounts: [accounts.bsc.privateKey]
+        // accounts: [accounts.bsc.privateKey]
     },
     bsc: {
         url: "https://bsc-dataseed1.defibit.io/",
         chainId: 56,
         gasPrice: 20000000000,
-        accounts: [accounts.bsc.privateKey]
+        // accounts: [accounts.bsc.privateKey]
     },
     polygon: {
         url: "https://polygon-rpc.com",
         chainId: 137,
         gasPrice: 53000000000,
-        accounts: [accounts.bsc.privateKey]
+        // accounts: [accounts.bsc.privateKey]
     },
     mumbai: {
         url: "https://rpc-mumbai.maticvigil.com/",
         chainId: 80001,
         gasPrice: 20000000000,
-        accounts: [accounts.bsc.privateKey]
+        // accounts: [accounts.bsc.privateKey]
     },
       test: {
       url: "https://bsctestapi.terminet.io/rpc",
       chainId: 97,
       gasPrice: 530000000000,
-      accounts: [accounts.bsc.privateKey]
+      // accounts: [accounts.bsc.privateKey]
     },
 };
 
@@ -50,6 +50,6 @@ module.exports = {
     },
     networks: networks,
     etherscan: {
-        apiKey: accounts.ether
+        // apiKey: accounts.ether
     }
 };
